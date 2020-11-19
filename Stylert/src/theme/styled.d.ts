2f7848d235df155c7d037c4592b1a17bd6606e94
Helena Strada <h.strada@hotmail.com>
2020-11-19 18:06:35 -0300

    feat: add font to theme

--- Stylert/src/theme/styled.d.ts
+++ Stylert/src/theme/styled.d.ts
@@ -2,8 +2,11 @@ import 'styled-components';
 
 import {colors} from './colors';
 
+import {fonts} from './fonts';
+
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: typeof colors;
+    fonts: typeof fonts;
   }
 }
