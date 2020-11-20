470750627ef8f319d359e49861c853c98905ee86
hstrada <h.strada@hotmail.com>
2020-11-20 17:42:39 -0300

    fix: bracket spacing

--- Stylert/App.tsx
+++ Stylert/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {SafeAreaView, View, StatusBar} from 'react-native';
-import {ThemeProvider} from 'styled-components';
-import {Card} from './src/components';
-import {stylertTheme} from './src/theme';
+import { SafeAreaView, View, StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { Card } from './src/components';
+import { stylertTheme } from './src/theme';
 
 const App = () => {
   return (
